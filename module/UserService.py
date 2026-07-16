3d3e9cb46acc20d054a2fcf1692a25dbcfa8f7bc
Vikas10112005 <vv992685@gmail.com>
2026-07-16 23:45:13 +0530

    done

---
+++ module/UserService.py
@@ -0,0 +1,23 @@
+# def Add(firstName, lastName):
+#     print("Adding User")
+#     print("Name", firstName, lastName)
+#     return
+#
+#
+# def Update(firstName, lastName):
+#     print("Update User")
+#     print("Name", firstName, lastName)
+#     return
+
+def Add(firstname,lastname):
+    print("Adding User")
+    print("name",firstname,lastname)
+    return
+
+
+def Update(firstname,lastname):
+    print("Update User")
+    print("name",firstname,lastname)
+    return
+
+
---
+++ module/testUserService.py
@@ -0,0 +1,10 @@
+# import UserService
+#
+#
+# UserService.Add("Ram","Sharma")
+# UserService.Update("Shyam", "Verma")
+
+import UserService
+
+UserService.Add("ram","sharma")
+UserService.Update("shyam","sharma")
